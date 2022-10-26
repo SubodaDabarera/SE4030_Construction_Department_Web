57b2294372908681e990dd4764105e1cea8a1c52
Ramath <ramathmanjitha@gmail.com>
2022-10-26 11:26:39 +0530

    Admin layout testin stage 1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import AdminLayout from "./layouts/AdminLayout";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./authentication/Login";
 
@@ -8,6 +9,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/login" element={<Login />} />
+          <Route path="/admin" element={<AdminLayout />} />
         </Routes>
       </Router>
     </div>
