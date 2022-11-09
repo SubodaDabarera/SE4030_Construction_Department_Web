3fdd650c45984f0d79de2d267ddde6ea9f75758f
Suboda Dabarera <88880898+SubodaDabarera@users.noreply.github.com>
2022-11-10 00:56:22 +0530

    partial approved, sent to top manager added

--- src/pages/pManager/RequestListNew.jsx
+++ src/pages/pManager/RequestListNew.jsx
@@ -11,10 +11,12 @@ import {
 } from "@mui/material";
 import {
   getRequestsListByStatus,
+  updatePartialOrderQty,
   updateRequestStatus,
 } from "../../api/orderAPI";
 import RequestRow from "../../components/procurementManager/RequestRow";
 import { getUserDetails } from "../../api/userAPI";
+import { async } from "@firebase/util";
 
 export default function RequestListNew() {
   const [productList, setProductList] = useState([]);
@@ -24,6 +26,10 @@ export default function RequestListNew() {
   const [userDetails, setUserDetails] = useState();
   const [isAcceptClicked, setIsAcceptClicked] = useState(false);
   const [isRejectClicked, setIsRejectClicked] = useState(false);
+  const [partialOpen, setPartialOpen] = useState(false);
+  const [sendTopManagerOpen, setSendTopManagerOpen] = useState(false);
+  const [approvedQuantity, setApprovedQuantity] = useState(0);
+  const [isAppovedQuantity, setIsAppovedQuantity] = useState(false);
 
   useEffect(() => {
     async function getData() {
@@ -34,8 +40,7 @@ export default function RequestListNew() {
     getData();
   }, []);
 
-  const handleOpen = async (product) => {
-    console.log(product);
+  const handleOpen = async (product, button) => {
     await viewProduct(product.productId, setProductDetails).then(() => {
       console.log("Data retrieved successfully");
     });
@@ -43,7 +48,13 @@ export default function RequestListNew() {
       console.log("Data retrieved successfully");
     });
 
-    setOpen(true);
+    if (button == "approve" || button == "reject") {
+      setOpen(true);
+    } else if (button == "partial") {
+      setPartialOpen(true);
+    } else if (button == "topManager") {
+      setSendTopManagerOpen(true);
+    }
   };
 
   const handleClose = async (status, orderId) => {
@@ -62,7 +73,7 @@ export default function RequestListNew() {
         console.log("Data retrieved successfully");
       });
     }
-    getData();
+    await getData();
 
     setOpen(false);
   };
@@ -71,16 +82,73 @@ export default function RequestListNew() {
     if (decision == "approved") {
       setIsAcceptClicked(true);
       setIsRejectClicked(false);
-      handleOpen(product);
+      handleOpen(product, "approve");
     } else if (decision == "rejected") {
       setIsRejectClicked(true);
       setIsAcceptClicked(false);
-      handleOpen(product);
+      handleOpen(product, "reject");
     }
+  };
 
-    // await updateRequestStatus({ orderId, updateOrder: decision }).then(() => {
-    //   console.log("Order details updated");
-    // });
+  const onPartialApprove = async (product) => {
+    if (approvedQuantity > 0) {
+      if (product.quantity >= approvedQuantity) {
+        await updatePartialOrderQty(
+          product._id,
+          approvedQuantity,
+          product.unitPrice
+        )
+          .then(() => {
+            console.log("Partially approved");
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+
+        await updateRequestStatus({
+          orderId: product._id,
+          updateOrder: "partiallyApproved",
+        })
+          .then(() => {
+            console.log("Qty updated in partially approved order");
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+
+        await getRequestsListByStatus(setRequestsList).then(() => {
+          console.log("Data retrieved successfully");
+        });
+
+        setIsAppovedQuantity(false);
+        setPartialOpen(false);
+        setApprovedQuantity(0);
+      } else {
+        // add error message here...
+      }
+    } else {
+      setIsAppovedQuantity(true);
+      setPartialOpen(true);
+    }
+  };
+
+  const onSendToTopManager = async (product) => {
+    await updateRequestStatus({
+      orderId: product._id,
+      updateOrder: "topManager",
+    })
+      .then(() => {
+        console.log("order updated");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    await getRequestsListByStatus(setRequestsList).then(() => {
+      console.log("Data retrieved successfully");
+    });
+
+    setSendTopManagerOpen(false);
   };
 
   return (
@@ -116,13 +184,13 @@ export default function RequestListNew() {
                         scope="col"
                         className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
                       >
-                        Unit Price ($)
+                        Unit Price (LKR)
                       </th>
                       <th
                         scope="col"
                         className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
                       >
-                        Total amount ($)
+                        Total amount (LKR)
                       </th>
                       <th
                         scope="col"
@@ -174,71 +242,19 @@ export default function RequestListNew() {
                             </button>
                           </div>
                           <button
-                            onClick={() => handleOpen(product)}
+                            onClick={() => handleOpen(product, "partial")}
                             className="p-1 my-1 bg-slate-300 rounded-md text-slate-800 hover:bg-gray-400"
                           >
                             Partially approve
                           </button>
 
                           <button
-                            onClick={() => handleOpen(product)}
-                            className="p-1 bg-slate-300 rounded-md text-slate-800 hover:bg-gray-400"
+                            onClick={() => handleOpen(product, "topManager")}
+                            className="px-2 p-1 bg-slate-300 rounded-md text-slate-800 hover:bg-gray-400"
                           >
                             Send to Top manager
                           </button>
                         </td>
-
-                        {/* <Dialog
-                          open={open}
-                          onClose={handleClose}
-                          aria-labelledby="alert-dialog-title"
-                          aria-describedby="alert-dialog-description"
-                          fullWidth={true}
-                        >
-                          <DialogTitle id="alert-dialog-title">
-                            <div className="align-middle py-4 justify-center grid font-semibold">
-                              Order Details
-                            </div>
-                          </DialogTitle>
-                          <DialogContent>
-                            <DialogContentText id="alert-dialog-description">
-                              {productDetails && (
-                                <>
-                                  <p className="align-middle text-gray-900 pb-4">
-                                    Title : {productDetails.title}
-                                  </p>
-                                  <p className="align-middle text-gray-900 pb-4">
-                                    Owner : {productDetails.owner}
-                                  </p>
-                                  <p className="align-middle text-gray-900 pb-4">
-                                    Price : {productDetails.unitPrice}
-                                  </p>
-                                  <p className="align-middle text-gray-900 pb-4">
-                                    Quantity : {product.quantity}
-                                  </p>
-                                  <p className="align-middle text-gray-900 pb-4">
-                                    Location : {productDetails.location}
-                                  </p>
-                                </>
-                              )}
-                              {userDetails && (
-                                <p className="align-middle text-gray-900 pb-4">
-                                  Seller name : {userDetails.userName}
-                                </p>
-                              )}
-                            </DialogContentText>
-                          </DialogContent>
-
-                          <DialogActions>
-                            <button
-                              type="button"
-                              className="inline-flex items-center justify-center rounded-md border border-transparent bg-yellow-600 mr-4 mb-4 text-white px-4 py-2 text-sm"
-                              onClick={() => handleClose()}
-                            >
-                              OK
-                            </button>
-                          </DialogActions>
-                        </Dialog> */}
 
                         {isAcceptClicked ? (
                           <>
@@ -353,6 +369,141 @@ export default function RequestListNew() {
                             </Dialog>
                           </>
                         )}
+
+                        {/* partial approve modal */}
+                        <Dialog
+                          open={partialOpen}
+                          onClose={() => setPartialOpen(false)}
+                          aria-labelledby="alert-dialog-title"
+                          aria-describedby="alert-dialog-description"
+                          fullWidth={true}
+                        >
+                          <DialogTitle id="alert-dialog-title">
+                            <div className="align-middle py-4 justify-center grid font-semibold">
+                              Partially approve order
+                            </div>
+                          </DialogTitle>
+                          <DialogContent>
+                            <DialogContentText id="alert-dialog-description">
+                              {productDetails && (
+                                <>
+                                  <p className="align-middle text-gray-900 pb-4 ml-4">
+                                    Title : {productDetails.title}
+                                  </p>
+                                  <p className="align-middle text-gray-900 pb-4 ml-4">
+                                    Owner : {productDetails.owner}
+                                  </p>
+                                  <p className="align-middle text-gray-900 pb-4 ml-4">
+                                    Price : {productDetails.unitPrice}.00
+                                  </p>
+                                  <p className="align-middle text-gray-900 pb-4 ml-4">
+                                    Quantity : {product.quantity}
+                                  </p>
+                                  <p className="align-middle text-gray-900 pb-4 ml-4">
+                                    Location : {productDetails.location}
+                                  </p>
+                                </>
+                              )}
+                              {userDetails && (
+                                <p className="align-middle text-gray-900 pb-4 ml-4">
+                                  Seller name : {userDetails.userName}
+                                </p>
+                              )}
+                              <p className="align-middle text-yellow-600 pb-4 ml-4 mt-4">
+                                Enter the approved quantity
+                              </p>
+                              <div className="mt-1">
+                                <input
+                                  id="quantity"
+                                  name="quantity"
+                                  type="number"
+                                  autoComplete="current-quantity"
+                                  placeholder="Enter quantity"
+                                  required
+                                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-yellow-500 focus:border-yellow-500 sm:text-sm"
+                                  onChange={(e) =>
+                                    setApprovedQuantity(e.target.value)
+                                  }
+                                />
+                              </div>
+                              {isAppovedQuantity && (
+                                <div className="bg-red-200 mt-1 w-full px-2 text-sm py-1 rounded-md text-red-700">
+                                  Please enter quantity
+                                </div>
+                              )}
+                            </DialogContentText>
+                          </DialogContent>
+
+                          <DialogActions>
+                            <button
+                              type="button"
+                              className="inline-flex w-full font-semibold items-center justify-center rounded-md border border-transparent bg-yellow-600 mx-10 mb-4 text-white px-4 py-2 text-sm"
+                              onClick={() =>
+                                // handleClose("approved", product._id)
+                                // setPartialOpen(false)
+                                onPartialApprove(product)
+                              }
+                            >
+                              Partialy approve
+                            </button>
+                          </DialogActions>
+                        </Dialog>
+
+                        {/* send to top manager */}
+                        <Dialog
+                          open={sendTopManagerOpen}
+                          onClose={() => setSendTopManagerOpen(false)}
+                          aria-labelledby="alert-dialog-title"
+                          aria-describedby="alert-dialog-description"
+                          fullWidth={true}
+                        >
+                          <DialogTitle id="alert-dialog-title">
+                            <div className="align-middle py-4 justify-center grid font-semibold">
+                              Order Details
+                            </div>
+                          </DialogTitle>
+                          <DialogContent>
+                            <DialogContentText id="alert-dialog-description">
+                              {productDetails && (
+                                <>
+                                  <p className="align-middle text-gray-900 pb-4 ml-4">
+                                    Title : {productDetails.title}
+                                  </p>
+                                  <p className="align-middle text-gray-900 pb-4 ml-4">
+                                    Owner : {productDetails.owner}
+                                  </p>
+                                  <p className="align-middle text-gray-900 pb-4 ml-4">
+                                    Price : {productDetails.unitPrice}.00
+                                  </p>
+                                  <p className="align-middle text-gray-900 pb-4 ml-4">
+                                    Quantity : {product.quantity}
+                                  </p>
+                                  <p className="align-middle text-gray-900 pb-4 ml-4">
+                                    Location : {productDetails.location}
+                                  </p>
+                                </>
+                              )}
+                              {userDetails && (
+                                <p className="align-middle text-gray-900 pb-4 ml-4">
+                                  Seller name : {userDetails.userName}
+                                </p>
+                              )}
+                            </DialogContentText>
+                          </DialogContent>
+
+                          <DialogActions>
+                            <button
+                              type="button"
+                              className="inline-flex w-full font-semibold items-center justify-center rounded-md border border-transparent bg-yellow-600 mx-10 mb-4 text-white px-4 py-2 text-sm"
+                              onClick={() =>
+                                // handleClose("approved", product._id)
+                                onSendToTopManager(product)
+                              }
+                            >
+                              Send to top manager
+                            </button>
+                          </DialogActions>
+                        </Dialog>
                       </tr>
                     ))}
                   </tbody>
